d4f28a7db603e2e6ea2505fa6be262f52d683bee
LHM777 <777harvey777@gmail.com>
2021-06-30 21:48:08 +0800

    added the main method

--- src/PercentageCalculatorMain.java
+++ src/PercentageCalculatorMain.java
@@ -2,5 +2,15 @@ package com.lhm777.percentagecalculator;
 
 
 public class PercentageCalculatorMain {
+	
+	public static void main(String args[]){
+    
+    
+      PercentageCalculatorGUI x =   new PercentageCalculatorGUI();
+      x.setMinimumSize(x.getMinimumSize());
+      x.setVisible(true);
+      
+      
+    }
     
 }
