bb12303e71daa531bf03353ef2b2b57fc145f92f
LHM777 <777harvey777@gmail.com>
2021-07-05 22:26:48 +0800

    HandledException Class

--- src/HandledException.java
+++ src/HandledException.java
@@ -4,4 +4,31 @@ package com.lhm777.percentagecalculator;
 
 public class HandledException {
     
+	
+	//This class handles the Exceptions that are thrown by PercentageCalculator Class.
+    //Since there is a possibility of an exception being generated while calculation occurs, we would need to handle that.
+    //An exception would be generated when division by zero occurs.
+    //An exception would also be generated when infinity occurs.
+    //When that happens, we would throw a custom exception called 'HandledException'
+    //The HandledException class will identify what exception it is and then send back a String message as a response.
+    
+    private String code;
+
+    public HandledException(String code, String message) {
+        super(message);
+        this.setCode(code);
+    }
+
+    public HandledException(String code, String message, Throwable cause) {
+        super(message, cause);
+        this.setCode(code);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
